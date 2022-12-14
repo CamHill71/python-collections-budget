ded641afbd48c838dfad91ac8207bde747c2669b
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-15 06:30:40 +0800

    s

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -58,7 +58,7 @@ def main():
 
     fig,ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
-    values = [myBudgetList.sum_expenses,myBudgetList.overages,myBudgetList.budget]
+    values = [myBudgetList.sum_expenses,myBudgetList.sum_overages ,myBudgetList.budget]
     ax.bar(labels,values,color=['green','red','blue'])
     ax.set_title('Your total expenses vs. total budget')
     plt.show()
